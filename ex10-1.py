0cd9104251be0696a47ceb18b6915d6b46991bfc
tsaoyuhsi <tsaoyuhsi@gmail.com>
2025-12-22 19:15:10 +0800

    Create ex10-1.py

---
+++ ex10-1.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+fruits[2] = "grape"
+
+print(fruits[2])
+print(fruits)
+
+fruits.append("hello")
+print(fruits)
